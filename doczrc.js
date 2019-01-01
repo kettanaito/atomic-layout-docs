3a9d4a9b0d9e3fc3f8e2e92f95175c8fbcc5ccc8
Artem Zakharchenko <kettanaito@gmail.com>
2019-01-01 16:21:33 +0100

    Sets "base" to "atomic-layout-docs"

--- doczrc.js
+++ doczrc.js
@@ -4,6 +4,7 @@ import merge from 'webpack-merge'
 export default {
   lang: 'en',
   dest: '/docs',
+  base: '/atomic-layout-docs/',
   title: 'Atomic layout',
   description: 'Documentation on Atomic layout library',
   favicon: 'assets/favicon.png',
